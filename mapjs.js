16c8e225b864fcf264e6eb0917e514d31b2df8e9
Matteo Giaimo <mgiaimo@hawk.iit.edu>
2017-06-29 19:16:36 -0500

    new info windows
    i created more info windows for the markers when u click on them

--- mapjs.js
+++ mapjs.js
@@ -1,8 +1,15 @@
 var map;
-var club = '<strong>Baia Le Grotte</strong> ' +
-                'Night Club that is on the beach';
-var pizza = '<strong>Ristorante La Montagnola</strong> ' +
-            'come try out the towns best pizza!'
+var club = '<strong>Baia Le Grotte ' +
+                'Night Club that is on the beach</strong>';
+var pizza = '<strong>Ristorante La Montagnola ' +
+            'come try out the towns best pizza!</strong>'
+var mondellobeach = '<strong>Mondello Beach is a beautiful ' 
+                    + 'location not to far from Palermo city.</strong>'
+var sanvito = '<strong>San Vito Lo Capo is known as '
+              + 'one of the beautifuliest beaches in the world.</strong>'
+var scopello = '<strong>Cala Mazzo di Sciacca just ' 
+                + 'another famous beach of Sicily</strong>'
+        
         function initMap() {
         // Create a map object and specify the DOM element for display.
         var map = new google.maps.Map(document.getElementById('map'), {
@@ -33,7 +40,7 @@ var pizza = '<strong>Ristorante La Montagnola</strong> ' +
 
 
 
-         var marker = new google.maps.Marker ({
+         var mondello = new google.maps.Marker ({
             position: {lat: 38.200241, lng: 13.328579},
             map: map,
             title: 'Mondello Palace',
@@ -42,7 +49,19 @@ var pizza = '<strong>Ristorante La Montagnola</strong> ' +
             
         });
 
-          var marker = new google.maps.Marker ({
+         var mondelloMarker = new google.maps.InfoWindow({
+          content: mondellobeach
+          });
+
+          mondello.addListener('click', function() {
+          mondelloMarker.open(map, mondello);
+
+          setTimeout(function () { mondelloMarker.close(); }, 10000);
+          });
+
+
+
+          var sanvitobeach = new google.maps.Marker ({
             position: {lat: 38.176601, lng: 12.738661},
             map: map,
             title: 'San Vito Lo Capo',
@@ -50,7 +69,17 @@ var pizza = '<strong>Ristorante La Montagnola</strong> ' +
             animation: google.maps.Animation.DROP,
         });
 
-          var marker = new google.maps.Marker ({
+          var sanvitoMarker = new google.maps.InfoWindow({
+          content: sanvito
+          });
+
+          sanvitobeach.addListener('click', function() {
+          sanvitoMarker.open(map, sanvitobeach);
+
+          setTimeout(function () { sanvitoMarker.close(); }, 10000);
+          });
+
+          var scopellobeach = new google.maps.Marker ({
             position: {lat: 38.079734, lng: 12.811925},
             map: map,
             title: 'Cala Mazzo di Sciacca',
@@ -58,6 +87,17 @@ var pizza = '<strong>Ristorante La Montagnola</strong> ' +
             animation: google.maps.Animation.DROP,
             
         });
+
+           var scopelloMarker = new google.maps.InfoWindow({
+          content: scopello
+          });
+
+          scopellobeach.addListener('click', function() {
+          scopelloMarker.open(map, scopellobeach);
+
+          setTimeout(function () { scopelloMarker.close(); }, 10000);
+          });
+
 
           var marker = new google.maps.Marker ({
             position: {lat: 38.048775, lng: 12.999010},
@@ -78,6 +118,8 @@ var pizza = '<strong>Ristorante La Montagnola</strong> ' +
 
           setTimeout(function () { infowindow.close(); }, 10000);
           });
+
+          google.maps.event.addDomListener(window, 'load', init);
 
 
         }
